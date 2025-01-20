c5b2606c5b4c1200ba5ecf9da8705ab783e0d6c6
maundu55 <marymaundu.mm@gmail.com>
2025-01-20 16:51:28 +0300

    add filter functionality

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Heart } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const Recipe = ({recipe}) => {
   return (
-    <div >
-
+    <div className='relative mb-4 rounded-lg bg-white shadow-md'>
+        <button className='absolute left-4 top-4 z-20 flex size-[32px] cursor-pointer items-center justify-center rounded-full bg-primary-400 text-white'>
+            <Heart />
+        </button>
+        <Link to={`/recipe/${recipe.id}`}>
+        <img src={recipe.image} alt={recipe.title}  className='h-48 w-full rounded-t-lg object-cover'/>
+        <div className='p-4'>
+            <h2 className='text-400 font-600'>{recipe.title}</h2>
+            <div className='flex mt-2 items-center text-sm text-gray-600'>
+            <span>{recipe.time}</span>
+            <span>{recipe.difficulty}</span>
+            </div>
+            <p className='text-200 mt-2'>{recipe.description}</p>
+        </div>
+        </Link>
     </div>
   )
 }
--- src/sections/Recipes.jsx
+++ src/sections/Recipes.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Search } from 'lucide-react'
 import {recipes } from '../data'
 import Recipe from '../components/Recipe'
+import NotFound from '../assets/not-found.png'
 
 const Recipes = () => {
+    const[searchTerm, setSearchTerm] = useState('fas');
+const handleChange =(e)=>{
+    setSearchTerm(e.target.value)
+    // console.log(e)
+}
+
+const filteredRecipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
    <section className='mx-auto max-w-6xl px-4 py-8'>
     <div className='md:flex md:justify-between md:w-full md:items-center'>
         <div className='relative mb-4 w-1/2 md:mb-0'>
-            <input type="text" placeholder='Search recipes...' className='w-full rounded-lg border border-gray-100 p-4 pl-12 focus:outline-none focus:ring-1 focus:ring-primary-400' />
+            <input
+            type="text" 
+            value={searchTerm}
+            onChange={handleChange}
+            placeholder='Search recipes...' 
+            className='w-full rounded-lg border border-gray-100 p-4 pl-12 focus:outline-none focus:ring-1 focus:ring-primary-400'
+            />
+
             <Search size={20} className='absolute left-4 top-1/2 -translate-y-1/2 transform text-gray-400' />
         </div>
         <div>
-            <button className="font-500 rounded-lg bg-primary-400 px-4 py-2 text-white">All recipes</button>
+            <button className="font-500 rounded-lg bg-primary-400 px-4 py-2 text-white">Liked recipes</button>
         </div>
     </div>
-    {
-        recipes.map(recipe =>(
+   <div>
+  {
+    filteredRecipes.length === 0? (
+        <div>
+            <img className='mx-auto size-[400px]' src={NotFound} alt="not found" />
+            <p className='text-500 font-600 text-center'>No recipes found</p>
+        </div>
+    ) : (
+        <div className='mt-8 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-3'>
+         {
+        filteredRecipes.map(recipe =>(
             <Recipe key={recipe.id} recipe={recipe} />
         ))
     }
+        </div>
+    )
+  }
+   </div>
    </section>
   )
 }
