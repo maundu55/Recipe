c2bdeeb992cf028f8087b4fe431aa03f8fb96431
maundu55 <marymaundu.mm@gmail.com>
2025-01-31 17:51:26 +0300

    add ingredient and info

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,31 +1,58 @@
 import { useParams } from "react-router-dom"
 import { recipes } from '../data'
+import { useState } from "react";
 
 
 
 const RecipeDetails = () => {
     const { id } = useParams();
+    const [selected, setSelected] = useState('information')
 
     const recipe = recipes.find(recipe=> recipe.id === id)
   return (
-    <div className="relative">
-      <div className="absolute left-0 top-0  h-full w-full bg-black opacity-80" />
+    <div>
+      <div className="relative">
+        <div className="absolute left-0 top-0  h-full w-full bg-black opacity-80" />
 
-      <div className="container flex h-[60vh] flex-col items-start justify-center">
-        <div className="z-40 mr-auto max-w-[700px] pt-16 text-white">
-          <h2 className="text-500 font-700 md:text-800 mb-1 capitalize leading-[1.2]">
-            {recipe.title}
-          </h2>
+        <div className="container flex h-[60vh] flex-col items-start justify-center">
+          <div className="z-40 mr-auto max-w-[700px] pt-16 text-white">
+            <h2 className="text-500 font-700 md:text-800 mb-1 capitalize leading-[1.2]">
+              {recipe.title}
+            </h2>
+          </div>
+
+          <img
+            className="absolute left-0 -z-10 h-full w-full object-cover"
+            src={recipe.image}
+            alt={recipe.title}
+          />
         </div>
+      </div>
+      <div className="container py-12">
+        <div className="md:grid md:grid-cols-2 md:gap-8">
+          <div className="mb-4 md:mb-0">
+            <img className="rounded-md" src={recipe.image} alt={recipe.title} />
+          </div>
+          <div>
+            <div className="space-x-4">
+                <button className={`${selected === 'information' ? 'font-500 rounded-lg bg-primary-400 px-4 py-2 text-white' : 'rounded-lg border-neutral-900 px-4 py-2'}`} onClick={()=> setSelected('information')} >Information</button>
+                <button  className={`${selected === 'ingredients' ? 'font-500 rounded-lg bg-primary-400 px-4 py-2 text-white' : 'rounded-lg border-neutral-900 px-4 py-2'}`} onClick={()=> setSelected('ingredients')} >Ingredients</button>
+            </div>
+            <div className="mt-4">
+                {selected === 'information' && <p>{recipe.information}</p>}
 
-        <img
-          className="absolute left-0 -z-10 h-full w-full object-cover"
-          src={recipe.image}
-          alt={recipe.title}
-        />
+                {selected === 'ingredients' && (
+                    <ul>
+                    {recipe.ingredients.map((ingredient, index)=>(
+                        <li key={index}>{ingredient}</li>
+                    ))}
+                    </ul>)}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default RecipeDetails
