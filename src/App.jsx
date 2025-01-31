0c5a7fb918f892fff0e130de87b799c1be2f63a9
maundu55 <marymaundu.mm@gmail.com>
2025-01-31 19:13:54 +0300

    add fotter section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import RecipeDetails from './pages/RecipeDetails'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path='/' element={<Home />} />        
         <Route path='recipe/:id' element={<RecipeDetails />} />        
       </Routes>
+      <Footer />
       </BrowserRouter>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+  return (
+    <footer>
+        <hr className="text-gray-100"/>
+        <p className="p-8 text-center">
+        Copyright {new Date().getFullYear()} - Feast
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
