954434b6a7a7a00f8c66eac95791f9be85473325
maundu55 <marymaundu.mm@gmail.com>
2025-01-31 17:54:56 +0300

    add testimonial section

---
+++ src/sections/Testimonial.jsx
@@ -0,0 +1,14 @@
+import { testimonialData } from "../data"
+
+
+const Testimonial = () => {
+  return (
+    <section>
+        <div className="container">
+            <h2>What Our Users Are Saying</h2>
+        </div>
+    </section>
+  )
+}
+
+export default Testimonial
